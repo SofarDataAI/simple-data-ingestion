2deba6a04a9ff514b78accbb918141a7bf573f7a
KennyDizi <xamarindevelopervietnam@outlook.com>
2024-04-21 11:37:41 +0700

    Retreive s3 buckets from existing ones

--- lib/simple-data-ingestion-stack.ts
+++ lib/simple-data-ingestion-stack.ts
@@ -1,16 +1,14 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
 import { SimpleDataIngestionStackProps } from './SimpleDataIngestionStackProps';
 
 export class SimpleDataIngestionStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props: SimpleDataIngestionStackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'SimpleDataIngestionQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const s3UnstructuredDataBucket = s3.Bucket.fromBucketName(this, `${props.resourcePrefix}-unstructuredDataBucket`, props.unstructuredDataBucketName);
+    const s3GoldenDatasetBucket = s3.Bucket.fromBucketName(this, `${props.resourcePrefix}-goldenDatasetBucket`, props.goldenDatasetBucketName);
+    const s3EmbeddingDatasetBucket = s3.Bucket.fromBucketName(this, `${props.resourcePrefix}-embeddingDatasetBucket`, props.embeddingDatasetBucketName);
   }
 }
