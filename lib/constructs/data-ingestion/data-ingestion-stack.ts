0c6eb0ab912b51837e42d8dd43307f4942449dae
KennyDizi <xamarindevelopervietnam@outlook.com>
2024-04-22 18:44:04 +0700

    Removed unused files

---
+++ lib/constructs/data-ingestion/data-ingestion-stack.ts
@@ -0,0 +1,61 @@
+import * as cdk from "aws-cdk-lib";
+import * as path from "path";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import { NestedStack } from "aws-cdk-lib";
+import { DataIngestionStackProps } from "./DataIngestionStackProps";
+import { Construct } from "constructs";
+import { LlrtFunction } from "cdk-lambda-llrt";
+import { OutputFormat } from "aws-cdk-lib/aws-lambda-nodejs";
+
+export class DataIngestionStack extends NestedStack {
+  constructor(scope: Construct, id: string, props: DataIngestionStackProps) {
+    super(scope, id, props);
+
+    const uploadingBucket = props.uploadingBucket;
+
+    // define the data ingestion stack here
+    // llrt lambda to generante presigned url for s3
+    // lambda function url
+    // authentication with Cognito
+
+    const dataIngestionLambdaFn = new LlrtFunction(this, `${props.resourcePrefix}-dataIngestionLambdaFn`, {
+      functionName: `${props.resourcePrefix}-dataIngestionLambdaFn`,
+      runtime: cdk.aws_lambda.Runtime.NODEJS_20_X,
+      entry: path.join(__dirname, '../src/lambdas/message-ingestion/index.ts'),
+      handler: 'handler',
+      environment: {
+          S3_BUCKET_NAME: uploadingBucket.bucketName,
+      },
+      llrtVersion: 'latest',
+      role: new cdk.aws_iam.Role(this, `${props.resourcePrefix}-dataIngestionLambdaFn-Role`, {
+          assumedBy: new cdk.aws_iam.ServicePrincipal('lambda.amazonaws.com'),
+          managedPolicies: [
+              cdk.aws_iam.ManagedPolicy.fromAwsManagedPolicyName('service-role/AWSLambdaBasicExecutionRole'),
+          ],
+      }),
+      timeout: cdk.Duration.seconds(60), // 60 seconds
+      architecture: lambda.Architecture.ARM_64,
+      logGroup: new cdk.aws_logs.LogGroup(this, `${props.resourcePrefix}-dataIngestionLambdaFn-LogGroup`, {
+          logGroupName: `${props.resourcePrefix}-dataIngestionLambdaFn-LogGroup`,
+          removalPolicy: cdk.RemovalPolicy.DESTROY,
+          retention: cdk.aws_logs.RetentionDays.ONE_WEEK,
+      }),
+      memorySize: 1024,
+      bundling: {
+          minify: true,
+          sourceMap: true,
+          sourcesContent: false,
+          esbuildVersion: '0.20.2',
+          target: 'es2020',
+          format: OutputFormat.ESM,
+          forceDockerBundling: true,
+      },
+      awsSdkConnectionReuse: false, // https://speedrun.nobackspacecrew.com/blog/2024/03/13/lambda-environment-variables-impact-on-coldstarts.html#does-it-impact-you
+      projectRoot: path.join(__dirname, '../src/lambdas/message-ingestion'),
+      depsLockFilePath: path.join(__dirname, '../src/lambdas/message-ingestion/package-lock.json'),
+    });
+
+    // grant permissions to the lambda function
+    uploadingBucket.grantWrite(dataIngestionLambdaFn);
+  }
+}
