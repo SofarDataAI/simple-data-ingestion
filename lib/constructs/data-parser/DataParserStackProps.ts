37cae7017acd502ebd1a9a7ca6f9e79fe8f2ba7d
KennyDizi <xamarindevelopervietnam@outlook.com>
2024-04-22 22:28:51 +0700

    Add DataParserStackProps

--- lib/constructs/data-parser/DataParserStackProps.ts
+++ lib/constructs/data-parser/DataParserStackProps.ts
@@ -5,4 +5,6 @@ import { SimpleDataIngestionBaseStackProps } from "../../SimpleDataIngestionStac
 export interface DataParserStackProps extends NestedStackProps, SimpleDataIngestionBaseStackProps {
     readonly s3UnstructuredDataBucket: cdk.aws_s3.IBucket;
     readonly s3GoldenDatasetBucket: cdk.aws_s3.IBucket;
+    readonly s3UnstructuredFileUploadQueue: cdk.aws_sqs.Queue;
+    readonly s3GoldenDatasetFileUploadQueue: cdk.aws_sqs.Queue;
 }
